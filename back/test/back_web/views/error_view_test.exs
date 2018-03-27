c3bc490f3173026aba628896f3d7820ee57de215
Anton Satin <satinad@gmail.com>
2018-03-27 18:11:22 +0300

    initial and backend

---
+++ back/test/back_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule BackWeb.ErrorViewTest do
+  use BackWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(BackWeb.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(BackWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(BackWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
---
+++ back/test/back_web/views/layout_view_test.exs
@@ -0,0 +1,3 @@
+defmodule BackWeb.LayoutViewTest do
+  use BackWeb.ConnCase, async: true
+end
