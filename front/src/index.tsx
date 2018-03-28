6d741758078eb8542fc7e92999d13a1e4a4933de
Anton Satin <satinad@gmail.com>
2018-03-28 21:55:23 +0300

    added apollo client

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,8 +4,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import { ApolloProvider } from 'react-apollo';
+import apolloClient from './utils/apolloClient';
+
+function getRoot() {
+  const root = document.getElementById('root');
+  if (root == null) {
+    throw new Error('No root element');
+  }
+  return root;
+}
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>,
+  getRoot()
 );
+
 registerServiceWorker();
---
+++ front/src/utils/apolloClient/index.ts
@@ -0,0 +1,11 @@
+
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({ uri: '/graphql' });
+
+export default new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
